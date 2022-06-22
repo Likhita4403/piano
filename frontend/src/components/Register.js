c9f0d616b6b0903a4cc85faed4bc92e7ad310f46
Likhita <likhitabasuthkar@gmail.com>
2022-06-22 16:45:50 +0530

    changes in signup

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 import { useState, useEffect } from "react";
 import Axios from "axios";
+import './Signup.css';
 
 function Register() {
   const [listOfUsers, setListOfUsers] = useState([]);
   const [name, setname] = useState("");
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
-  // const [confirm_Password, setconfirm_Password] = useState("");
   useEffect(() => {
     Axios.get("http://localhost:3001/getUsers").then((response) => {
       setListOfUsers(response.data);
@@ -18,72 +18,61 @@ function Register() {
     Axios.post("http://localhost:3001/writeUsers", {
       name,
       email,
-      password
-      // confirm_Password,
+      password,
     }).then((response) => {
       setListOfUsers([
         ...listOfUsers,
         {
           name,
           email,
-          password
-          // confirm_Password,
+          password,
         },
       ]);
     });
   };
 
   return (
-    <div className='container mx-5 text-center border border-primary rounded '>
-      {/* <div className="UsersDisplay">
-        {listOfUsers.map((user) => {
-          return (
-            // <div>
-            //   <h1>name: {user.name}</h1>
-            //   <h1>email: {user.email}</h1>
-            //   <h1>password: {user.password}</h1>
-            //   {/* <h1>Confirm password: {user.confirm_Password}</h1>
-            // </div>
-          );
-        }
-        )};
-     </div> */}
-      <div className='p-5'>
-      <label className='lead m-3'>Name</label>
-        <input className='m-3'
-          type="text"
-          onChange={(event) => {
-            setname(event.target.value);
-          }}
-        />
-        <br/>
-        <label className='lead m-3'>Email</label>
-        <input 
-          type="text" className=''
-          onChange={(event) => {
-            setemail(event.target.value);
-          }}
-        />
-        <br/>
-        <label className='lead m-3'>Password</label>
-        <input  className='m-3'
-          type="text"
-          onChange={(event) => {
-            setpassword(event.target.value);
-          }}
-        />
-        {/* <input
-          type="text"
-          placeholder="Confirm password..."
-          onChange={(event) => {
-            setconfirm_Password(event.target.value);
-          }}
-        />
-        {password===confirm_Password} */}
-        <br/>
-        <button className='my-3 p-3 btn btn-lg btn-primary' onClick={writeUsers}> Create User </button>
+    <div className="w-50 container-fluid">
+      <div className="me-5 my-3 text-center ">
+        <div className="border border-primary rounded py-5 ">
+          <label className="display-6 align-baseline">Name</label>
+          <input
+            className="m-3 c w-50"
+            type="text"
+            onChange={(event) => {
+              setname(event.target.value);
+            }}
+          />
+          <br />
+          <label className="display-6 align-baseline">Email</label>
+          <input
+            type="text"
+            className="m-3 c w-50"
+            onChange={(event) => {
+              setemail(event.target.value);
+            }}
+          />
+          <br />
+          <label className="display-6 align-baseline">Password</label>
+          <input
+            className="m-3 c w-50"
+            type="text"
+            onChange={(event) => {
+              setpassword(event.target.value);
+            }}
+          />
+          <br />
+          <button
+            className="my-3 p-3 btn btn-lg btn-primary"
+            onClick={writeUsers}
+          >
+            {" "}
+            Create User{" "}
+          </button>
+        </div>
       </div>
     </div>
-);}
+  );
+}
 
-export default Register
+export default Register;
